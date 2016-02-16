8c207de256112eef51379898447aa061ba10a583
John Skilbeck <john.skilbeck@gmail.com>
2016-02-16 15:20:28 -0800

    parsed

---
+++ lib/vantiv_emf_record_types.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+
+def get_record_type(record):
+    return record[9:12]
+
+def show_record_types_in_file(inputFile):
+    recordTypes = set()
+
+    for line in inputFile:
+        recordTypes.add(get_record_type(line))
+
+    print "Records in ", inputFile.name
+    print sorted(recordTypes)
+    print
+    return
+
+def mask_071_record(record):
+    start = record[:73]
+    return start + ('*' * (len(record) - len(start))) + '\n'
+
+def mask_072_record(record):
+    return record[:15] + ('*' * 26) + record[41:]
+
+def mask_300_record(record):
+    return record[:56] + ('*' * 6) + record[62:] #Currently only shows last 3 for Amex.
+
+def mask_501_record(record):
+    start = record[:15]
+    return start + ('*' * (len(record) - len(start))) + '\n'
+
+def mask_emf(inputFile):
+    KNOWN_RECORDS = {'010', '070', '071', '072', '080', '300', '301', '500', '501', '503', '910', '970', '980'}
+    NEEDS_MASKING = {'071': mask_071_record, '072': mask_072_record, '300': mask_300_record, '501': mask_501_record}
+
+    splitFileName = os.path.splitext(inputFile.name)
+    outputFileName = splitFileName[0] + '.masked' + splitFileName[1]
+
+    with open(outputFileName, 'w') as outputFile:
+        for line in inputFile:
+            recordType = get_record_type(line)
+            if recordType in KNOWN_RECORDS:
+                maskedLine = line
+                if recordType in NEEDS_MASKING:
+                    maskedLine = NEEDS_MASKING[recordType](line)
+                outputFile.write(maskedLine)
+            else:
+                print "Unknown record type ", recordType
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fileNames', nargs='+', help='Vantiv eMAF (EMF) file(s) to process.')
+    parser.add_argument('-r', '--record-types-only', action='store_true', 
+        help='Only lists records contained in Vantiv eMAF (EMF) file. Does not mask files.')
+
+    args = parser.parse_args()
+
+    for fileName in args.fileNames:
+        with open(fileName, 'r') as inputFile:
+            if args.record_types_only:
+                show_record_types_in_file(inputFile)
+            else:
+                mask_emf(inputFile)
+
--- parser.js
+++ parser.js
@@ -1,22 +1,7 @@
-var fs = require('fs'),
-	path = require('path'),
-	h = require('./lib/helper.js'),
-	fileName = 'sample.emaf.txt',
-	file = path.join('./data', fileName),
-	stream = fs.createReadStream(file),
-	rl = require('readline').createInterface({ input: stream })
-	;
-
-var result = {
-		id: null,
-		record_type: null,
-		date: null,
-		card: null,
-		network: null,
-		amount: null,
-		interchange_amt: null,
-		surcharge_amt: null
-	},
+var fs = require('fs'), path = require('path'),	h = require('./lib/helper.js'),
+	fileName = 'sample.emaf.txt', file = path.join('./data', fileName),
+	stream = fs.createReadStream(file), rl = require('readline').createInterface({ input: stream }),
+	result = {	id: null	}, merchant = { merchant_id: null },
 	data = [];
 
 rl.on('line', function(line){
@@ -47,6 +32,9 @@ var line_type = function(line, cb){
 		case record_type_identifier == 301:
 			description = 'credit reconciliation transaction 2';
 			break;
+		case record_type_identifier == 70:
+			description = 'mid 1';
+			break;
 		case record_type_identifier >= 100 && record_type_identifier <= 300:
 			description = 'settlement detail';
 			break;
@@ -62,6 +50,8 @@ var line_type = function(line, cb){
 			credit_reconciliation_transaction_1(line, cb); break;
 		case 'credit reconciliation transaction 2':
 			credit_reconciliation_transaction_2(line, cb); break;
+		case 'mid 1':
+			mid_1(line, cb); break;			
 		case 'settlement detail':
 		case 'settlement summary':
 			break;
@@ -75,47 +65,71 @@ var new_record = function(){
 	result = {};
 };
 
+var new_merchant = function(){
+	merchant = {};
+};
+
+var mid_1 = function(line, cb){
+	var merchant_account_number = h.parse(line,18,16),
+		division_number = h.parse(line,50,3),
+		store = h.parse(line,53,9),
+		merchant_name = h.parse(line,62,25)
+	;
+	new_merchant();
+
+	merchant.merchant_id = merchant_account_number;
+	merchant.division = division_number;
+	merchant.store = store;
+	merchant.merchant_name = merchant_name;
+	// cb();
+};
+
 var credit_reconciliation_transaction_1 = function(line, cb){
 	var record_sequence_number = h.parse(line,1,9),
-		record_type_identifier = h.parse(line,10,3),
 		transaction_date = h.parse(line,16,8),
 		record_length = h.parse(line,13,3),
-		transaction_type_code = h.parse(line,37,2),
+		transaction_type_code = h.parse(line,37,2), // table 100.4
+		transaction_type = h.transaction_type(transaction_type_code),
 		card_number = h.parse(line,51,19),
-		network = h.parse(line,111,4),
+		network = h.parse(line,111,4), // table 100.14
+		card_type = h.parse(line,115,3), //table 100.31
 		txn_amount = parseFloat(h.parse(line,74,11) * 0.01).toFixed(2),
 		mcc = h.parse(line,107,4),
 		draft_locator = h.parse(line,124,11)
 	;
-
 	new_record();
 
 	result.id = record_sequence_number;
-	result.record_type = record_type_identifier;
 	result.date = h.date(transaction_date);
+	result.transaction_type = transaction_type;
 	result.card = card_number;
-	result.network = network;
+	result.network = h.network(network);
 	result.amount = txn_amount;
 	result.mcc = mcc;
+	result.card_type = card_type;
 
 	result.transferLog = h.parse(draft_locator, 2 , draft_locator.length - 3 -1 ) +':' + h.parse(draft_locator,draft_locator.length-2,3);
 
+	result.merchant_id = merchant.merchant_id;
+	result.merchant_name = merchant.merchant_name;
 	// cb();
 };
 
 
 var credit_reconciliation_transaction_2 = function(line, cb){
 	var record_sequence_number = h.parse(line,1,9),
-		record_type_identifier = h.parse(line,10,3),
 		interchange_amt = parseFloat(h.parse(line,61,14)* 0.000000001).toFixed(2),
 		interchange_sign = h.parse(line,75,1),
-		surcharge_amt = parseFloat(h.parse(line,79,8)* 0.01).toFixed(2)
+		surcharge_amt = parseFloat(h.parse(line,79,8)* 0.01).toFixed(2),
+
+		interchange_code = h.parse(line,52,9) // table 100.29
 	;
 
 	result.id = record_sequence_number;
-	result.record_type = record_type_identifier;
 	result.interchange_amt = interchange_sign + interchange_amt;
 	result.surcharge_amt = interchange_sign + surcharge_amt;
+
+	result.interchange_code = interchange_code;
 
 	data.push(result);
 
